0bdae929cad70bf57e3939669a3a6b38a6dbaa44
MARTIN WICKETT <martintwickett@gmail.com>
2024-01-26 17:39:53 -0500

    Created Drinks model

--- north_kingstown/models.py
+++ north_kingstown/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Drinks(models.Model):
+    name = models.CharField(max_length=50)
+    alcohol = models.CharField(max_length=50)
+    bottle_size = models.IntegerField()
+    price = models.DecimalField(max_digits=6,decimal_places=2)

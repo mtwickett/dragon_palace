f2417255643227c61af791e9bf7510d107c591a7
MARTIN WICKETT <martintwickett@gmail.com>
2024-02-10 13:27:23 -0500

    Changed from sqlite to postgres db

--- dragon_palace/settings.py
+++ dragon_palace/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'dragon_palace.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dragon_palace',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'boston'
     }
 }
 
